38d76c033a8519c3e4aace0fd5836c437632018e
Aimable <aimable.niyogakiza@drpgroup.com>
2019-04-04 20:10:32 +0100

    event listerners

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@ const scoreDash = document.querySelector(".scoreDash");
 const lifeDash = document.querySelector(".lifeDash");
 const btnStart = document.querySelector(".btnStart");
 const container = document.getElementById('container');
+
+btnStart.addEventListener('click', startGame);
+btnStart.addEventListener('keydown', pressKeyOn);
+btnStart.addEventListener('keyup', pressKeyOff);
+
 let gamePlay = false;
 let animationGame = requestAnimationFrame(playGame);
 
@@ -13,6 +18,23 @@ let keys = {
   ArrowRight: false,
   ArrowLeft: false,
 };
+
+function startGame(){
+  let div = document.createElement('div');
+  div.setAttribute('class', 'playerCar');
+  div.x = 250;
+  div.y = 500;
+  container.appendChild(div);
+  gamePlay = true
+}
+
+function pressKeyOn(){
+
+}
+
+function pressKeyOff() {
+
+}
 
 function playGame(){
   console.log('car race')
